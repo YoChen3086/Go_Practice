551a7826d2d820067c2a69a151c5df62a612a2f1
yochen20 <d87487575107@gmail.com>
2019-04-12 13:35:18 +0800

    變數

---
+++ variable.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// var x, y, z int
+// var c, python, java bool
+
+var x, y, z int = 1, 2, 3
+var c, python, java = true, false, "no!"
+
+// var (
+// 	x int
+// 	y int
+// 	z int
+// 	c bool
+// 	python bool
+// 	java bool
+// )
+
+func main() {
+	// var x, y, z int = 1, 2, 3
+	// c, python, java := true, false, "no!"
+
+	fmt.Println(x, y, z, c, python, java)
+}
